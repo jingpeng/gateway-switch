848b450874f932a718750b7369b13e41a61aa43f
王劲鹏 <jingpeng.wang@outlook.com>
2016-07-09 00:40:11 +0800

    TCPClient加入网关上下线和设备注册数据测试

--- gateway/src/edu/tongji/GatewayServer.java
+++ gateway/src/edu/tongji/GatewayServer.java
@@ -8,21 +8,44 @@ public class GatewayServer {
 
 	private static final int SERVER_PORT = 6600;
 
+	private static final int UPLOAD_DATA_COUNT = 13;
+
 	public void receive() {
+		ServerSocket serverSocket = null;
 		try {
-			ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+			serverSocket = new ServerSocket(SERVER_PORT);
 			Socket client = serverSocket.accept();
+			int count = 0;
+			int[] data = new int[UPLOAD_DATA_COUNT];
 			while (true) {
-				int data = client.getInputStream().read();
-				if (data != -1) {
-					System.out.println(data);
-				} else {
-					break;
+				int value = client.getInputStream().read();
+				if (value != -1) {
+					data[count] = value;
+					count++;
+				}
+				if (count == UPLOAD_DATA_COUNT) {
+					count = 0;
+					uploadData(data);
 				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			if (serverSocket != null) {
+				try {
+					serverSocket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
+	}
+
+	private void uploadData(int[] data) {
+		for (int i = 0; i != data.length; i++) {
+			System.out.print(data[i]);
+		}
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
--- gateway/src/edu/tongji/LockClient.java
+++ gateway/src/edu/tongji/LockClient.java
@@ -4,19 +4,34 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class LockClient {
 	private static final String SERVER_IP = "127.0.0.1";
 
 	private static final int SERVER_PORT = 6600;
 
-	private byte[] data = { 0X7F, 0X7F, 0X6F };
+	private byte[] gatewayOnlineData = { 0X7F, 0X7F, // 开始标志
+			(byte) 0XFF, (byte) 0XFF, // 设备区域ID
+			0X30, // 上线或下线
+			0X00, 0X00, 0X00, 0X00, // 空
+			0X6F, 0X6F, // CRC数据校验
+			0X0D, 0X0A // 结束标志
+	};
+
+	private byte[] deviceRegisterData = { 0X7F, 0X7F, // 开始标志
+			(byte) 0XFF, (byte) 0XFF, // 设备区域ID
+			0X30, // 上线或下线
+			0X00, 0X00, 0X00, 0X00, // 空
+			0X6F, 0X6F, // CRC数据校验
+			0X0D, 0X0A // 结束标志
+	};
 
 	public void connect() {
 		try {
 			Socket socket = new Socket(InetAddress.getByName(SERVER_IP), SERVER_PORT);
 			OutputStream out = socket.getOutputStream();
-			out.write(data);
+			out.write(concatAll(gatewayOnlineData, deviceRegisterData));
 			socket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -26,5 +41,19 @@ public class LockClient {
 	public static void main(String[] args) {
 		LockClient client = new LockClient();
 		client.connect();
+	}
+
+	private static byte[] concatAll(byte[] first, byte[]... rest) {
+		int totalLength = first.length;
+		for (byte[] array : rest) {
+			totalLength += array.length;
+		}
+		byte[] result = Arrays.copyOf(first, totalLength);
+		int offset = first.length;
+		for (byte[] array : rest) {
+			System.arraycopy(array, 0, result, offset, array.length);
+			offset += array.length;
+		}
+		return result;
 	}
 }
